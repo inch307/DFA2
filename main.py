923a37be60beec651a4760bd764878eb0d73d798
home_inch <inch307@gmail.com>
2022-04-18 02:23:17 +0900

    erw

--- main.py
+++ main.py
@@ -44,6 +44,9 @@ def val(net, val_loader, device):
     net.eval()
     criterion = nn.CrossEntropyLoss()
     test_loss = 0
+    test_B_loss_1 = 0
+    test_B_loss_2 = 0
+    test_B_loss_3 = 0
     correct1 = 0
     correct3 = 0
     correct5 = 0
@@ -57,11 +60,17 @@ def val(net, val_loader, device):
             correct1 += c1
             correct3 += c3
             correct5 += c5
+            # test_B_loss = net.get_B_loss(output)
+            # test_B_loss_1 += test_B_loss[0] * 64 / len(val_loader.dataset)
+            # test_B_loss_2 += test_B_loss[1] * 64 / len(val_loader.dataset)
+            # test_B_loss_3 += test_B_loss[2] * 64 / len(val_loader.dataset)
+            
     acc1 = correct1 / len(val_loader.dataset) * 100
     acc3 = correct3 / len(val_loader.dataset) * 100
     acc5 = correct5 / len(val_loader.dataset) * 100
 
     print(f'validation, acc1: {acc1}, acc3: {acc3}, acc5: {acc5}')
+    # print(f'validation, acc1: {acc1}, acc3: {acc3}, acc5: {acc5}, test_B_loss_y1: {test_B_loss_1}, test_B_loss_y2: {test_B_loss_2}, test_B_loss_y3: {test_B_loss_3}')
 
 def train_backprop(net, train_loader, optimizer, device):
     net.train()
@@ -107,6 +116,9 @@ def train_dfa(net, train_loader, optimizer, device, lr):
     correct1 = 0
     correct3 = 0
     correct5 = 0
+    train_B_loss_1 = 0
+    train_B_loss_2 = 0
+    train_B_loss_3 = 0
     n_iter = 0
     for _batch_idx, (data, target) in enumerate(tqdm(train_loader)):
         data, target = data.to(device), target.to(device)
@@ -120,25 +132,9 @@ def train_dfa(net, train_loader, optimizer, device, lr):
         target = one_hot(target, 64, 10)
         net.backward(output-target ,data)
 
-        if n_iter == 0:
-            acc1_i = c1 / target.size(0) * 100
-            acc3_i = c3 / target.size(0) * 100
-            acc5_i = c5 / target.size(0) * 100
-            print(f'acc1: {acc1_i}, acc3: {acc3_i}, acc5: {acc5_i}, loss: {loss}')
-            print('asd')
-            print(output)
-            print(net.fc1.weight)
-            print(net.fc3.bias)
-
-        if n_iter == 1:
-            acc1_i = c1 / target.size(0) * 100
-            acc3_i = c3 / target.size(0) * 100
-            acc5_i = c5 / target.size(0) * 100
-            print(f'acc1: {acc1_i}, acc3: {acc3_i}, acc5: {acc5_i}, loss: {loss}')
-            print('asd')
-            print(output)
-            print(net.fc1.weight)
-            print(net.fc3.bias)
+        if n_iter % 30 == 0:
+            y1_loss, y2_loss, y3_loss = net.get_B_loss(output)
+            print(f'n_iter: {n_iter}, B_loss is: y1: {y1_loss}, y1: {y2_loss}, y1: {y3_loss}')
             
 
         optimizer.step()
@@ -151,11 +147,36 @@ def train_dfa(net, train_loader, optimizer, device, lr):
             acc5_i = c5 / target.size(0) * 100
             print(f'acc1: {acc1_i}, acc3: {acc3_i}, acc5: {acc5_i}, loss: {loss}')
         n_iter += 1
-    acc1 = correct1 / len(train_loader.dataset) * 100
-    acc3 = correct3 / len(train_loader.dataset) * 100
-    acc5 = correct5 / len(train_loader.dataset) * 100
+    
+    correct1 = 0
+    correct3 = 0
+    correct5 = 0
+    train_B_loss_1 = 0
+    train_B_loss_2 = 0
+    train_B_loss_3 = 0
+    with torch.no_grad():
+        for _batch_idx, (data, target) in enumerate(tqdm(train_loader)):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = net(data)
+            c1, c3, c5 = topk_correct(output, target, (1, 3, 5))
+            correct1 += c1
+            correct3 += c3
+            correct5 += c5
+            loss = criterion(output, target)
+            target = one_hot(target, 64, 10)
+            train_B_loss = net.get_B_loss(output)
+            train_B_loss_1 += train_B_loss[0] * 64 / len(train_loader.dataset)
+            train_B_loss_2 += train_B_loss[1] * 64 / len(train_loader.dataset)
+            train_B_loss_3 += train_B_loss[2] * 64 / len(train_loader.dataset)
+        print(f'train B_loss is: y1: {train_B_loss_1}, y1: {train_B_loss_2}, y1: {train_B_loss_3}')
+        acc1 = correct1 / len(train_loader.dataset) * 100
+        acc3 = correct3 / len(train_loader.dataset) * 100
+        acc5 = correct5 / len(train_loader.dataset) * 100
 
-    print(f'acc1: {acc1}, acc3: {acc3}, acc5: {acc5}')
+        print(f'acc1: {acc1}, acc3: {acc3}, acc5: {acc5}')
+
+    
     
 def train_dfa2(net, train_loader, optimizer, device, lr, post):
     net.train()
@@ -164,6 +185,9 @@ def train_dfa2(net, train_loader, optimizer, device, lr, post):
     correct1 = 0
     correct3 = 0
     correct5 = 0
+    train_B_loss_1 = 0
+    train_B_loss_2 = 0
+    train_B_loss_3 = 0
     n_iter = 0
     for _batch_idx, (data, target) in enumerate(tqdm(train_loader)):
         data, target = data.to(device), target.to(device)
@@ -177,11 +201,19 @@ def train_dfa2(net, train_loader, optimizer, device, lr, post):
         target = one_hot(target, 64, 10)
 
         if post:
+            if n_iter % 30 == 0:
+                y1_loss, y2_loss, y3_loss = net.get_B_loss(output)
+                print(f'n_iter: {n_iter}, B_loss is: y1: {y1_loss}, y1: {y2_loss}, y1: {y3_loss}')
             net.backward(output-target ,data)
-            net.update_B(lr, output-target)
+            # net.update_B(lr, output-target)
+            net.new_update_B(lr, output)
 
         else:
+            if n_iter % 30 == 0:
+                y1_loss, y2_loss, y3_loss = net.get_B_loss(output)
+                print(f'n_iter: {n_iter}, B_loss is: y1: {y1_loss}, y1: {y2_loss}, y1: {y3_loss}')
             net.update_B(lr, output-target)
+            # net.new_update_B(lr, output)
             net.backward(output-target ,data)
 
         optimizer.step()
@@ -194,11 +226,34 @@ def train_dfa2(net, train_loader, optimizer, device, lr, post):
             acc5_i = c5 / target.size(0) * 100
             print(f'acc1: {acc1_i}, acc3: {acc3_i}, acc5: {acc5_i}, loss: {loss}')
         n_iter += 1
-    acc1 = correct1 / len(train_loader.dataset) * 100
-    acc3 = correct3 / len(train_loader.dataset) * 100
-    acc5 = correct5 / len(train_loader.dataset) * 100
 
-    print(f'acc1: {acc1}, acc3: {acc3}, acc5: {acc5}')
+    correct1 = 0
+    correct3 = 0
+    correct5 = 0
+    train_B_loss_1 = 0
+    train_B_loss_2 = 0
+    train_B_loss_3 = 0
+    with torch.no_grad():
+        for _batch_idx, (data, target) in enumerate(tqdm(train_loader)):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = net(data)
+            c1, c3, c5 = topk_correct(output, target, (1, 3, 5))
+            correct1 += c1
+            correct3 += c3
+            correct5 += c5
+            loss = criterion(output, target)
+            target = one_hot(target, 64, 10)
+            train_B_loss = net.get_B_loss(output)
+            train_B_loss_1 += train_B_loss[0] * 64 / len(train_loader.dataset)
+            train_B_loss_2 += train_B_loss[1] * 64 / len(train_loader.dataset)
+            train_B_loss_3 += train_B_loss[2] * 64 / len(train_loader.dataset)
+        print(f'train B_loss is: y1: {train_B_loss_1}, y1: {train_B_loss_2}, y1: {train_B_loss_3}')
+        acc1 = correct1 / len(train_loader.dataset) * 100
+        acc3 = correct3 / len(train_loader.dataset) * 100
+        acc5 = correct5 / len(train_loader.dataset) * 100
+
+        print(f'acc1: {acc1}, acc3: {acc3}, acc5: {acc5}')
 
 def train_dfa3(net, train_loader, optimizer, device, lr, epochs):
     net.train()
@@ -219,8 +274,14 @@ def train_dfa3(net, train_loader, optimizer, device, lr, epochs):
         loss = criterion(output, target)
         target = one_hot(target, 64, 10)
 
+        
+        if n_iter % 30 == 0:
+            y1_loss, y2_loss, y3_loss = net.get_B_loss(output)
+            print(f'n_iter: {n_iter}, B_loss is: y1: {y1_loss}, y1: {y2_loss}, y1: {y3_loss}')
+        # for i in range(epochs):
+        #     net.update_B(lr, output-target)
         for i in range(epochs):
-            net.update_B(lr, output-target)
+            net.new_update_B(lr, output)
         net.backward(output-target ,data)
 
         optimizer.step()
@@ -233,11 +294,34 @@ def train_dfa3(net, train_loader, optimizer, device, lr, epochs):
             acc5_i = c5 / target.size(0) * 100
             print(f'acc1: {acc1_i}, acc3: {acc3_i}, acc5: {acc5_i}, loss: {loss}')
         n_iter += 1
-    acc1 = correct1 / len(train_loader.dataset) * 100
-    acc3 = correct3 / len(train_loader.dataset) * 100
-    acc5 = correct5 / len(train_loader.dataset) * 100
+    
+    correct1 = 0
+    correct3 = 0
+    correct5 = 0
+    train_B_loss_1 = 0
+    train_B_loss_2 = 0
+    train_B_loss_3 = 0
+    with torch.no_grad():
+        for _batch_idx, (data, target) in enumerate(tqdm(train_loader)):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = net(data)
+            c1, c3, c5 = topk_correct(output, target, (1, 3, 5))
+            correct1 += c1
+            correct3 += c3
+            correct5 += c5
+            loss = criterion(output, target)
+            target = one_hot(target, 64, 10)
+            train_B_loss = net.get_B_loss(output)
+            train_B_loss_1 += train_B_loss[0] * 64 / len(train_loader.dataset)
+            train_B_loss_2 += train_B_loss[1] * 64 / len(train_loader.dataset)
+            train_B_loss_3 += train_B_loss[2] * 64 / len(train_loader.dataset)
+        print(f'train B_loss is: y1: {train_B_loss_1}, y1: {train_B_loss_2}, y1: {train_B_loss_3}')
+        acc1 = correct1 / len(train_loader.dataset) * 100
+        acc3 = correct3 / len(train_loader.dataset) * 100
+        acc5 = correct5 / len(train_loader.dataset) * 100
 
-    print(f'acc1: {acc1}, acc3: {acc3}, acc5: {acc5}')
+        print(f'acc1: {acc1}, acc3: {acc3}, acc5: {acc5}')
 
 def main():
     args = parser.parse_args()
@@ -257,7 +341,7 @@ def main():
         net = DFANet(device=device)
         # optimizer = torch.optim.SGD(net.parameters(), lr=0.001)
         optimizer = torch.optim.RMSprop(net.parameters(), lr=0.001)
-        net.init('zero')
+        # net.init('zero')
         for i in range(args.epochs):
             train_dfa(net, train_loader, optimizer, device, lr)    
             val(net, val_loader, device)
@@ -265,7 +349,7 @@ def main():
         net = DFA2Net(device=device)
         # optimizer = torch.optim.SGD(net.parameters(), lr=0.001)
         optimizer = torch.optim.RMSprop(net.parameters(), lr=0.001)
-        net.init('zero')
+        # net.init('zero')
         for i in range(args.epochs):
             train_dfa2(net, train_loader, optimizer, device, lr, args.post)    
             val(net, val_loader, device)
--- net.py
+++ net.py
@@ -1,3 +1,4 @@
+from ssl import HAS_TLSv1_3
 import torch.nn as nn
 import torch
 
@@ -67,18 +68,18 @@ class DFANet(nn.Module):
 
         dx1 = torch.matmul(e, self.B1) * (1-torch.tanh(self.y1) ** 2)
         # print(dx1.shape)
-        self.fc1.weight.grad = torch.matmul(torch.t(dx1), x)
+        self.fc1.weight.grad = torch.matmul(torch.t(dx1), x) / 64 # batch szie
         # self.fc1.bias.grad = torch.sum(dx1, 0)
 
         dx2 = torch.matmul(e, self.B2) * (1-torch.tanh(self.y2) ** 2)
-        self.fc2.weight.grad = torch.matmul(torch.t(dx2), self.z1)
+        self.fc2.weight.grad = torch.matmul(torch.t(dx2), self.z1) / 64 # batch szie
         # self.fc2.bias.grad = torch.sum(dx2, 0)
 
         dx3 = torch.matmul(e, self.B3) * (1-torch.tanh(self.y3) ** 2)
-        self.fc3.weight.grad = torch.matmul(torch.t(dx3), self.z2)
+        self.fc3.weight.grad = torch.matmul(torch.t(dx3), self.z2) / 64 # batch szie
         # self.fc3.bias.grad = torch.sum(dx3, 0)
 
-        self.out.weight.grad = torch.matmul(torch.t(e), self.z3)
+        self.out.weight.grad = torch.matmul(torch.t(e), self.z3) / 64 # batch szie
         # self.out.bias.grad = torch.sum(e, 0)
 
         return
@@ -86,13 +87,24 @@ class DFANet(nn.Module):
     def init(self, method):
         if method == 'zero':
             self.fc1.weight.data.fill_(0)
-            self.fc1.bias.data.fill_(0)
+            # self.fc1.bias.data.fill_(0)
             self.fc2.weight.data.fill_(0)
-            self.fc2.bias.data.fill_(0)
+            # self.fc2.bias.data.fill_(0)
             self.fc3.weight.data.fill_(0)
-            self.fc3.bias.data.fill_(0)
+            # self.fc3.bias.data.fill_(0)
             self.out.weight.data.fill_(0)
-            self.out.bias.data.fill_(0)
+            # self.out.bias.data.fill_(0)
+
+    def get_B_loss(self, output):
+        y1_hat = self.softmax(torch.matmul(self.z1, torch.t(self.B1)))
+        y2_hat = self.softmax(torch.matmul(self.z2, torch.t(self.B2)))
+        y3_hat = self.softmax(torch.matmul(self.z3, torch.t(self.B3)))
+
+        y1_loss = torch.sum((output - y1_hat)**2) / 64
+        y2_loss = torch.sum((output - y2_hat)**2) / 64
+        y3_loss = torch.sum((output - y3_hat)**2) / 64
+
+        return y1_loss, y2_loss, y3_loss
 
 class DFA2Net(nn.Module):
     def __init__(self, *args, **kwargs):
@@ -137,22 +149,41 @@ class DFA2Net(nn.Module):
         
         return
 
+    def new_update_B(self, lr, output):
+        y1_hat = self.softmax(torch.matmul(self.z1, torch.t(self.B1)))
+        y2_hat = self.softmax(torch.matmul(self.z2, torch.t(self.B2)))
+        y3_hat = self.softmax(torch.matmul(self.z3, torch.t(self.B3)))
+
+        e1 = (output - y1_hat)
+        e2 = (output - y2_hat)
+        e3 = (output - y3_hat)
+
+        d1 = torch.t(e1 * y1_hat * (1 - y1_hat))
+        d2 = torch.t(e2 * y2_hat * (1 - y2_hat))
+        d3 = torch.t(e3 * y3_hat * (1 - y3_hat))
+
+        self.B1 = self.B1*(1- lr*0.01/64) - lr * torch.matmul(d1, self.z1) / 64 # batch szie
+        self.B2 = self.B2*(1- lr*0.01/64) - lr * torch.matmul(d2, self.z2) / 64
+        self.B3 = self.B3*(1- lr*0.01/64) - lr * torch.matmul(d3, self.z3) / 64
+        
+        return
+
     def backward(self, e, x):
         x = x.view(-1, 784)
 
         dx1 = torch.matmul(e, self.B1) * (1-torch.tanh(self.y1) ** 2)
-        self.fc1.weight.grad = torch.matmul(torch.t(dx1), x)
+        self.fc1.weight.grad = torch.matmul(torch.t(dx1), x) / 64 # batch szie
         # self.fc1.bias.grad = torch.sum(dx1, 0)
 
         dx2 = torch.matmul(e, self.B2) * (1-torch.tanh(self.y2) ** 2)
-        self.fc2.weight.grad = torch.matmul(torch.t(dx2), self.z1)
+        self.fc2.weight.grad = torch.matmul(torch.t(dx2), self.z1) / 64 # batch szie
         # self.fc2.bias.grad = torch.sum(dx2, 0)
 
         dx3 = torch.matmul(e, self.B3) * (1-torch.tanh(self.y3) ** 2)
-        self.fc3.weight.grad = torch.matmul(torch.t(dx3), self.z2)
+        self.fc3.weight.grad = torch.matmul(torch.t(dx3), self.z2) / 64 # batch szie
         # self.fc3.bias.grad = torch.sum(dx3, 0)
 
-        self.out.weight.grad = torch.matmul(torch.t(e), self.z3)
+        self.out.weight.grad = torch.matmul(torch.t(e), self.z3) / 64 # batch szie
         # self.out.bias.grad = torch.sum(e, 0)
 
         return
@@ -160,10 +191,21 @@ class DFA2Net(nn.Module):
     def init(self, method):
         if method == 'zero':
             self.fc1.weight.data.fill_(0)
-            self.fc1.bias.data.fill_(0)
+            # self.fc1.bias.data.fill_(0)
             self.fc2.weight.data.fill_(0)
-            self.fc2.bias.data.fill_(0)
+            # self.fc2.bias.data.fill_(0)
             self.fc3.weight.data.fill_(0)
-            self.fc3.bias.data.fill_(0)
+            # self.fc3.bias.data.fill_(0)
             self.out.weight.data.fill_(0)
-            self.out.bias.data.fill_(0)
+            # self.out.bias.data.fill_(0)
+
+    def get_B_loss(self, output):
+        y1_hat = self.softmax(torch.matmul(self.z1, torch.t(self.B1)))
+        y2_hat = self.softmax(torch.matmul(self.z2, torch.t(self.B2)))
+        y3_hat = self.softmax(torch.matmul(self.z3, torch.t(self.B3)))
+
+        y1_loss = torch.sum((output - y1_hat)**2) / 64
+        y2_loss = torch.sum((output - y2_hat)**2) / 64
+        y3_loss = torch.sum((output - y3_hat)**2) / 64
+
+        return y1_loss, y2_loss, y3_loss
